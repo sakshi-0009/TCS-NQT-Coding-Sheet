a57785dc2cb014c450c5c787a70da596b62eef3e
sakshi-0009 <sakshigome1809@gmail.com>
2023-07-26 22:22:25 +0530

    Smallest Number in the array

---
+++ TCS Coding Sheet/Array/01_Smallest_Number.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+class SmallestNumber{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter array size: ");
+        int size = Integer.parseInt(br.readLine());
+        int arr[] = new int[size];
+
+        System.out.println("Enter array elements: ");
+        for(int i=0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int min = arr[0];
+        for(int i=0; i<arr.length; i++){
+            if(min>arr[i]){
+                min = arr[i];
+            }
+        }
+        System.out.println("Smallest number is : "+min);
+    }
+}
