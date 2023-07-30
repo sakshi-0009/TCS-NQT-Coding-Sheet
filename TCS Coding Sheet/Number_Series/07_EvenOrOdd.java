78c490250c6e026f258cecd6a6196a9468b9b382
sakshi-0009 <sakshigome1809@gmail.com>
2023-07-30 09:52:02 +0530

    Even or Odd

---
+++ TCS Coding Sheet/Number_Series/07_EvenOrOdd.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class EvenOrOdd {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter number : ");
+        int N = Integer.parseInt(br.readLine());
+
+        if(N%2 == 0){
+            System.out.println(N+" is an even number");
+        }else if(N%2 != 0){
+            System.out.println(N+" is an odd number");
+        }else{
+            System.out.println("please enter valid Integer number.");
+        }
+
+    }
+}
