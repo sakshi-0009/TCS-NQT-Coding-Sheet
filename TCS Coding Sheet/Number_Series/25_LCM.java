401615c2e894d0d6621da107d88416a29c1dc3d6
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-13 12:01:12 +0530

    GCD of two numbers

--- TCS Coding Sheet/Number_Series/25_LCM.java
+++ TCS Coding Sheet/Number_Series/25_LCM.java
@@ -1,19 +1,20 @@
 import java.io.*;
 
-class GCD {
+class LCM {
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter num1 : ");
         int num1 = Integer.parseInt(br.readLine());
         System.out.println("Enter num2 : ");
         int num2 = Integer.parseInt(br.readLine());
-        
+
         int ans = 1;
         for (int i = 1; i <= Math.min(num1, num2); i++) {
             if (num1 % i == 0 && num2 % i == 0) {
-            ans = i;
+                ans = i;
+            }
         }
+        int lcm = (num1 * num2) / ans;
+        System.out.print("The LCM of the two given numbers is "+lcm);
     }
-    System.out.print("The GCD of the two number is "+ans);
-    }
-}   
+}
