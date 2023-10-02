fa2cb69747a24c6a0f93030b21049b28698d482f
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-10-03 00:25:53 +0530

    Palindrome String

--- TCS Coding Sheet/String/01_Palindrome.java
+++ TCS Coding Sheet/String/01_Palindrome.java
@@ -4,9 +4,11 @@ import java.io.*;
 
 class Palindrome {
 	public static void main(String[] args) throws IOException{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter String : ");
 		String str = br.readLine();
+		
 		String rev = "";
 		for (int i = str.length() - 1; i >= 0; i--) {
 			rev = rev + str.charAt(i);
