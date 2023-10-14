775426c3f63b50aff47b932f062add8e9db783b6
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-10-14 23:15:25 +0530

    Average of all the elements in the array

--- TCS Coding Sheet/Array/09_AvgOfElememts.java
+++ TCS Coding Sheet/Array/09_AvgOfElememts.java
@@ -1,6 +1,7 @@
 import java.io.*;
 
 class AvgOfElememts {
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter array size : ");
